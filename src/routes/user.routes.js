93827661f74a5fec1247b54f77ee1db76c0a94a8
Purakh-Nath <purakhnath45@gmail.com>
2026-04-27 00:20:16 +0530

    auth done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,6 +16,12 @@ import { requireRole } from '../middlewares/role.middleware.js';
 
 const router = Router();
 
-// Your routes here
+router.use(authenticate);
+router.use(requireRole('admin'));
+
+// Routes
+router.get('/', listUsers);
+router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
 
 export default router;
